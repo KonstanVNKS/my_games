8ceca543b1fe460e4460351d1b69b2263c120817
Konstantinos <kostantinos.vanakas@ulb.be>
2023-11-09 21:07:27 +0100

    small changes for a better use for the user

--- VANAKAS_KONSTANTINOS.py
+++ VANAKAS_KONSTANTINOS.py
@@ -5,7 +5,8 @@ Matricule: <000565802>
 Date: 15/11/2023
 Gaol: Create a minesweeper game with a GUI and adapting difficulty by the use of a class
 !!!Most part of the code was reused from a last year project of the same game but without the difficulty choice and
-the bombs position changes and with the system of flags. So most of the code was adapted to the new requirements.!!!
+the bombs position changes and with the system of flags. So most of the code was adapted to the new requirements.
+The previous project was the first required in the course of INFO-F-106 of the academic year 2022/2023!!!
 """
 import string as s
 from random import *
